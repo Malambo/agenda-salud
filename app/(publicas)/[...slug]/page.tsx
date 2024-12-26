c88571cc8ae8595b89a6fc212a466af9fafb9e24
Malambo <bloxito@gmail.com>
2024-12-26 13:47:34 -0300

    Formato código y css

--- app/(publicas)/[...slug]/page.tsx
+++ app/(publicas)/[...slug]/page.tsx
@@ -1,26 +1,34 @@
-import api from "@/lib/api"
-import TituloPagina from "@/components/ui/TituloPagina"
-import TarjeteroEspecialidades from "@/components/TarjeteroEspecialidades"
+import api from '@/lib/api';
+import TituloPagina from '@/components/ui/TituloPagina';
+import TarjeteroEspecialidades from '@/components/TarjeteroEspecialidades';
 
 
 export default async function CategoriaPage({params}: {params: Promise<{slug: string}>}) {
-   
+
     const slug = (await params).slug[0]
     const {urlIcono, titulo, descripcion} = await api.traeCategoria('/' + slug)
-    
+
     return (
         <>
         <div className='flex flex-col gap-24 items-center'>
             <TituloPagina urlIcono={urlIcono} titulo={titulo} />
-            <p className='w-2/3 text-pretty text-lg'>
-                {descripcion}
-            </p>
+            <p className='w-2/3 text-pretty text-lg'>{descripcion}</p>
         </div>
 
         <div className='mt-44'>
-            {slug === 'centro-salud' && <p className='font-bold text-orange-500'>{slug.toLocaleUpperCase()}</p>}
+            {slug === 'centro-salud' && (
+            <p className='font-bold text-orange-500'>
+                {slug.toLocaleUpperCase()}
+            </p>
+            )}
+
             {slug === 'especialidades' && <TarjeteroEspecialidades />}
-            {slug === 'profesionales' && <p className='font-bold text-orange-500'>{slug.toLocaleUpperCase()}</p>}
+            
+            {slug === 'profesionales' && (
+            <p className='font-bold text-orange-500'>
+                {slug.toLocaleUpperCase()}
+            </p>
+            )}
         </div>
         </>
     )
