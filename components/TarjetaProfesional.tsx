de85f495774d552417715a9843ab6059b8cd0336
Malambo <bloxito@gmail.com>
2025-05-20 08:08:57 -0300

    cambios desde acá

--- components/TarjetaProfesional.tsx
+++ components/TarjetaProfesional.tsx
@@ -2,8 +2,7 @@
 import {
     Accordion,
     AccordionContent,
-    AccordionItem,
-    AccordionTrigger}       from "@/components/ui/accordion"
+    AccordionItem}       from "@/components/ui/accordion"
 import MiAccordionTrigger   from "@/components/ui/MiAccordionTrigger"
 import api from '@/lib/api'
 
@@ -15,32 +14,45 @@ interface TarjetaProfesionalProps {
 
 export default async function TarjetaProfesional({idProfesional}: TarjetaProfesionalProps) {
 
-    const profesionales = await api.listaProfesionales()
-    const nombreEspecialidades = profesional.especialidades.map(especialidad => {api.traeEspecialidad(especialidad)})
-    console.log (nombreEspecialidades)
+    const profesional = await api.traeProfesional(idProfesional)
+    const especialidades = await api.listaEspecialidades()
+    const centros = await api.listaCentros()
+
+    const especialidadesProfesional = profesional.especialidades.map(idEspecialidad => {
+        const especialidad = especialidades.find(e=>e.id===idEspecialidad)
+        return especialidad ? especialidad.nombre : ''
+    })
+
+    const centrosProfesional = profesional.centrosSalud.map((idCentro: string) => {
+        const centro = centros.find(c=>c.id===idCentro)
+        return centro ? centro.nombre : ''
+    })
+
 
     return (
         <Accordion type="single" collapsible>
-            <AccordionItem value={'3'}>
+            <AccordionItem value={'0'}>
                 <div className='sticky top-52 bg-gradient-to-r from-emerald-50 to-emerald-100'>
-                    <MiAccordionTrigger url={'/profesionales'} urlIcono={'/Profesionales.svg'} titulo={'Todos los profesionales'}/>
+                    <MiAccordionTrigger url={'/profesionales'} urlIcono={'/Profesionales.svg'} titulo={profesional.nombre}/>
                 </div>
                 <AccordionContent>
-                    <Accordion type="single" collapsible className='bg-[#e8faf0] rounded text-zinc-800 p-4'>
-                        {profesionales.map(profesional=>(
-                            <AccordionItem key={profesional.id} value={profesional.id}>
-                                <div className='flex gap-2 items-center'>
-                                    <User className='size-4'/>
-                                    <AccordionTrigger className='font-medium'>{profesional.nombre}</AccordionTrigger>
-                                </div>
-                                {profesional.especialidades.map(idProf=>(
-                                    <AccordionContent key={idProf} className='text-sm px-4'>
-                                        {especialidades.find(especialidad=>especialidad.id === idProf)?.nombre}
-                                    </AccordionContent>
-                                ))}
-                            </AccordionItem>
-                        ))}
-                    </Accordion>
+                    <div className='flex gap-8 justify-start'>
+                        <div className='flex flex-col justify-start font-bold'>
+                            {especialidadesProfesional.map((especialidadProfesional, index) => (
+                            <div key={index} className='flex justify-start'>
+                                <p className='text-sm text-left'>{especialidadProfesional}</p>
+                            </div>
+                            ))}
+                        </div>
+
+                        <div className='flex flex-col justify-start'>
+                            {centrosProfesional.map((centrosProfesional, index) => (
+                            <div key={index} className='flex justify-start'>
+                                <p className='text-sm text-left'>{centrosProfesional}</p>
+                            </div>
+                            ))}
+                        </div>
+                    </div>
                 </AccordionContent>
             </AccordionItem>
         </Accordion>
